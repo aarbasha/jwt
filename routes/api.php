4c86445535f1c74fddb053c161ad394cc7f7f8f0
Anas Arbasha <anas.arbasha.sy@gmail.com>
2022-08-16 20:58:18 +0300

    clear code

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JWTAuthMiddleware;
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 //composer require tymon/jwt-auth:dev-develop
 
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-], function ($router) {
+], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -23,8 +19,6 @@ Route::group([
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
 //--------------------------------------------------------------------------------
-
-
 Route::middleware([JWTAuthMiddleware::class])->group(function () {
     //All posts
     Route::get('posts', [PostController::class, 'getAllPost']);
@@ -37,6 +31,9 @@ Route::middleware([JWTAuthMiddleware::class])->group(function () {
     //delete post
     Route::post('post/{id}', [PostController::class, 'destroy']);
 });
-
 //--------------------------------------------------------------------------------
-Route::get('users', [UserController::class, 'getAllUser']);
+
+Route::middleware([JWTAuthMiddleware::class])->group(function () {
+
+    Route::get('users', [UserController::class, 'getAllUser']);
+});
